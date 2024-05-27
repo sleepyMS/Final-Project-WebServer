9fea685fc400b6660dc361449b0a7181f8e79557
sleepyMS <minseok011017@gmail.com>
2024-05-28 08:59:09 +0900

    set dir

---
+++ src/main/java/com/finalProject/finalProject/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.finalProject.finalProject.controller;
+
+import com.finalProject.finalProject.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/auth")
+public class UserController {
+
+    @PostMapping("/signUp")
+    public String signUp(@RequestBody UserDto requestBod, Model model) {
+
+        model.addAttribute(requestBod);
+        return "1";
+    }
+
+    @PostMapping("/signIn")
+    public String signIn(@RequestBody UserDto requestBod, Model model) {
+
+        model.addAttribute(requestBod);
+        return "1";
+    }
+
+}
---
+++ src/main/java/com/finalProject/finalProject/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.finalProject.finalProject.dto;
+
+import lombok.*;
+
+@Data
+public class UserDto {
+    public int num;
+    public String name;
+    public String email;
+    public String password;
+    public String mbti;
+    public String nick;
+}
