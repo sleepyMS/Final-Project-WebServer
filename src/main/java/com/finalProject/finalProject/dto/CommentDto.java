91a266648201bc821e624a818a3eb051c121c6a2
sleepyMS <minseok011017@gmail.com>
2024-05-28 21:35:51 +0900

    min commit

---
+++ src/main/java/com/finalProject/finalProject/dto/CommentDto.java
@@ -0,0 +1,11 @@
+package com.finalProject.finalProject.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private int idx;
+    private int postIdx;
+    private String mbti;
+    private String nick;
+}
