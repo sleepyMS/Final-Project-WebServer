3aaef8827e2ce1cf0c70a9c4c30444da8daf36d2
ParkJunHyung <wnsgud117117@naver.com>
2024-05-31 23:14:23 +0900

    jun commit

--- src/main/java/com/finalProject/finalProject/dao/PostDaoImpl.java
+++ src/main/java/com/finalProject/finalProject/dao/PostDaoImpl.java
@@ -5,7 +5,6 @@ import com.finalProject.finalProject.dto.PostDto;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public class PostDaoImpl implements PostDao {
