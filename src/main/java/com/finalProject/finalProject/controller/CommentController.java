b2aea4411f366e997f90d370b8f9f11b48c2f878
sleepyMS <minseok011017@gmail.com>
2024-05-31 20:44:11 +0900

    min commit

--- src/main/java/com/finalProject/finalProject/controller/CommentController.java
+++ src/main/java/com/finalProject/finalProject/controller/CommentController.java
@@ -1,43 +1,43 @@
-//package com.finalProject.finalProject.controller;
-//
-//import com.finalProject.finalProject.service.CommentService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//@RequestMapping("/post/comment")
-//public class CommentController {
-//
-//    @Autowired
-//    private CommentService commentService;
-//    @RequestMapping("/write")
-//    public String write(@RequestParam("postIdx") int postIdx,
-//                        @RequestParam("userIdx") int userIdx,
-//                        @RequestParam("content") String content) {
-//
-//        commentService.insertComment(postIdx, userIdx, content);
-//
-//        return "redirect:/post/read/" + postIdx;
-//    }
-//
-//    @RequestMapping("/update/{postIdx}/{commentIdx}")
-//    public String update(@PathVariable("postIdx") int postIdx,
-//                         @PathVariable("commentIdx") int commentIdx) {
-//
-//        commentService.updateComment(commentIdx);
-//
-//        return "redirect:/post/read/" + postIdx;
-//    }
-//
-//    @RequestMapping("/delete/{postIdx}/{commentIdx}")
-//    public String delete(@PathVariable("postIdx") int postIdx,
-//                         @PathVariable("commentIdx") int commentIdx) {
-//
-//        commentService.deleteComment(commentIdx);
-//
-//        return "redirect:/post/read/" + postIdx;
-//    }
-//}
+package com.finalProject.finalProject.controller;
+
+import com.finalProject.finalProject.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/post/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+    @RequestMapping("/write")
+    public String write(@RequestParam("postIdx") int postIdx,
+                        @RequestParam("userIdx") int userIdx,
+                        @RequestParam("content") String content) {
+
+        commentService.insertComment(postIdx, userIdx, content);
+
+        return "redirect:/post/read/" + postIdx;
+    }
+
+    @RequestMapping("/update/{postIdx}/{commentIdx}")
+    public String update(@PathVariable("postIdx") int postIdx,
+                         @PathVariable("commentIdx") int commentIdx) {
+
+        commentService.updateComment(commentIdx);
+
+        return "redirect:/post/read/" + postIdx;
+    }
+
+    @RequestMapping("/delete/{postIdx}/{commentIdx}")
+    public String delete(@PathVariable("postIdx") int postIdx,
+                         @PathVariable("commentIdx") int commentIdx) {
+
+        commentService.deleteComment(commentIdx);
+
+        return "redirect:/post/read/" + postIdx;
+    }
+}
--- src/main/java/com/finalProject/finalProject/dao/CommentDaoImpl.java
+++ src/main/java/com/finalProject/finalProject/dao/CommentDaoImpl.java
@@ -1,61 +1,61 @@
-//package com.finalProject.finalProject.dao;
-//
-//import com.finalProject.finalProject.dto.CommentDto;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service
-//public class CommentDaoImpl implements CommentDao {
-//    private List<CommentDto> db = new ArrayList<>();
-//    public CommentDaoImpl() {
-//        System.out.println("CommentDaoImple 객체 생성");
-//        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test1"));
-//        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test2"));
-//        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test3"));
-//    }
-//
-////    Create
-//    @Override
-//    public void insertComment(CommentDto CommentDto) { db.add(CommentDto); }
-//
-////    Read
-//    @Override
-//    public List<CommentDto> getCommentByPostIdx(int idx) {
-//        return db.stream()
-//                .filter(comment -> comment.getPostIdx() == idx)
-//                .collect(Collectors.toList());
-//    }
-//    @Override
-//    public List<CommentDto> getCommentByUserIdx(int idx) {
-//        return db.stream()
-//                .filter(comment -> comment.getPostIdx() == idx)
-//                .collect(Collectors.toList());
-//    }
-//
-////    Update
-//    @Override
-//    public void updateComment(CommentDto commentDto) {
-//        db.stream()
-//                .filter(comment -> comment.getIdx() == commentDto.getIdx())
-//                .findFirst()
-//                .ifPresent(comment -> comment.setContent(commentDto.getContent()));
-//    }
-//
-////    Delete
-//    @Override
-//    public void deleteComment(long idx) {
-//        db.stream()
-//                .filter(comment -> comment.getIdx() == idx)
-//                .findFirst()
-//                .ifPresent(db::remove);
-//    }
-//    @Override
-//    public void deleteCommentByPostIdx(long idx) {
-//        db = db.stream()
-//                .filter(comment -> comment.getPostIdx() != idx)
-//                .collect(Collectors.toList());
-//    }
-//}
+package com.finalProject.finalProject.dao;
+
+import com.finalProject.finalProject.dto.CommentDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentDaoImpl implements CommentDao {
+    private List<CommentDto> db = new ArrayList<>();
+    public CommentDaoImpl() {
+        System.out.println("CommentDaoImple 객체 생성");
+        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test1"));
+        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test2"));
+        db.add(new CommentDto(1, 1, 1, "ENFJ", "min", "test3"));
+    }
+
+//    Create
+    @Override
+    public void insertComment(CommentDto CommentDto) { db.add(CommentDto); }
+
+//    Read
+    @Override
+    public List<CommentDto> getCommentByPostIdx(int idx) {
+        return db.stream()
+                .filter(comment -> comment.getPostIdx() == idx)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public List<CommentDto> getCommentByUserIdx(int idx) {
+        return db.stream()
+                .filter(comment -> comment.getPostIdx() == idx)
+                .collect(Collectors.toList());
+    }
+
+//    Update
+    @Override
+    public void updateComment(CommentDto commentDto) {
+        db.stream()
+                .filter(comment -> comment.getIdx() == commentDto.getIdx())
+                .findFirst()
+                .ifPresent(comment -> comment.setContent(commentDto.getContent()));
+    }
+
+//    Delete
+    @Override
+    public void deleteComment(long idx) {
+        db.stream()
+                .filter(comment -> comment.getIdx() == idx)
+                .findFirst()
+                .ifPresent(db::remove);
+    }
+    @Override
+    public void deleteCommentByPostIdx(long idx) {
+        db = db.stream()
+                .filter(comment -> comment.getPostIdx() != idx)
+                .collect(Collectors.toList());
+    }
+}
