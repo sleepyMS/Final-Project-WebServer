b174027a9b2fd8aaa18dfe9747617553ee309dad
ParkJunHyung <wnsgud117117@naver.com>
2024-05-31 22:46:06 +0900

    jun commit

--- src/main/java/com/finalProject/finalProject/service/PostService.java
+++ src/main/java/com/finalProject/finalProject/service/PostService.java
@@ -11,11 +11,5 @@ public interface PostService {
     PostDto save(PostDto post);
     int count();
     boolean increaseLikes(int idx);
-    void createBoard(String title);
-    List<PostDto> getPostsByBoardId(int boardId);
 
-    List<String> getAllBoards(); // 모든 게시판 이름을 가져오는 메서드 추가
-    List<PostDto> getRecentPosts(); // 최근 게시글 목록을 가져오는 메서드 추가
-
-    List<PostDto> getAllPosts();
 }
